19eac6a593d8b2e2b838f3f37661f8573672f785
KKDR1998 <77356024+KKDR1998@users.noreply.github.com>
2021-04-06 10:07:48 -1000

    Week 13; In-class stuff done

--- HackwichTen/ViewController.swift
+++ HackwichTen/ViewController.swift
@@ -9,11 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       //Sets up the inital conditions for the app launch
+        
+        //Text for the label
+        myLabel.text = "Index One selected on the Segmented Control"
+        
+        
     }
+    
+    
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+    //switch statement
+        switch
+            segmentedControl
+                .selectedSegmentIndex {
+            //creating diferent segments and what they do
+        case 0:
+            myLabel.text = "Index One selected on the Segmented Control"
+            
+        case 1:
+            myLabel.text = "Index Two selected. I got this!"
+
+            
+        default:
+            break
+            
+        }
+    
+    
+    }
+    
 
 
 }
